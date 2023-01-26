aa6887f7b8237097fcee847f7fab4a40eaacbb3f
Muhamed Cham <2003m.cham@gmail.com>
2023-01-26 23:51:51 +0000

    first commit

---
+++ src/container/Findus/FindUs.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
+const FindUs = () => (
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div app__wrapper_info>
+      <SubHeading title='Contact'/>
+      <h1 className='headtext__cormorant' style={{marginBottom: '3rem'}}>Find Us</h1>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+        <p className='p__cormorant' style={{color: '#DCCA87', margin: '2rem 0'}}>Opening Hours</p>
+        <p className='p__opensans-nocap'>Mon - Fri: 08:00 am - 09:00 pm</p>
+        <p className='p__opensans-nocap'>Sat - Sun: 10:00 am - 05:00 pm</p>
+      </div>
+      <button className='custom-button' style={{marginTop: '2rem'}}>Visit Us</button>
+    </div>
+    <div  className='app__wrapper_img'>
+      <img src={images.findus} alt='findus'/>
+    </div>
+  </div>
+);
+
+export default FindUs;
---
+++ src/container/Intro/Intro.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import {BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
+import { meal } from '../../constants';
+import './Intro.css';
+
+const Intro = () => {
+const [playVideo, setPlayVideo] = React.useState(false);
+const vidRef = React.useRef();
+
+const handleVideo = () => {
+  setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+  if (playVideo) {
+    vidRef.current.pause();
+  } else {
+    vidRef.current.play();
+  }
+}
+  
+ return (
+  <div className='app__video'>
+    <video 
+    src={meal}
+    ref={vidRef} 
+    type='video/mp4'
+    loop
+    controls={false}
+    muted
+    />
+
+    <div className='app__video-overlay flex__center' style={playVideo ?  {background: "rgba(0, 0, 0, 0)", transition: "ease 0.15s"} : {background: "rgba(0, 0, 0, 0.65)", transition: "ease 0.15s"} }>
+      <div 
+      className='app__video-overlay_circle flex__center'
+      onClick={handleVideo}
+      >
+      {playVideo 
+        ? <BsPauseFill 
+         color='#fff'
+         fontSize={30}
+         />
+        
+        : <BsFillPlayFill 
+         color='#fff'
+         fontSize={30}
+         />
+      }
+      </div>
+
+    </div>
+  </div>
+)
+ }
+
+export default Intro;
